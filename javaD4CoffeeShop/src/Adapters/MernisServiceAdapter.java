923d4d8511c2c5f827e5e701b11ad9e1b772de2f
akkoc.erkan@gmail.com <akkoc.erkan@gmail.com>
2021-05-05 14:43:03 +0300

    Coffee Shop Project

---
+++ javaD4CoffeeShop/src/Adapters/MernisServiceAdapter.java
@@ -0,0 +1,29 @@
+package Adapters;
+
+import java.rmi.RemoteException;
+
+import Abstract.CustomerCheckService;
+import Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
+public class MernisServiceAdapter implements CustomerCheckService {
+
+	@Override
+	public boolean checkIfRealPerson(Customer customer) {
+		KPSPublicSoapProxy client = new KPSPublicSoapProxy();
+
+		Long kimlik = Long.parseLong(customer.getNationalityId(), 10);
+		int dogumYili = customer.getDateOfBirth().getYear();
+
+		try {
+			return client.TCKimlikNoDogrula(kimlik, customer.getFirstName().toUpperCase(),
+					customer.getLastName().toUpperCase(), dogumYili);
+		} catch (RemoteException e) {
+
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+}
---
+++ javaD4CoffeeShop/src/javaD4CoffeeShop/Main.java
@@ -0,0 +1,21 @@
+package javaD4CoffeeShop;
+
+import java.time.LocalDate;
+
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.save(new Customer(1,"Engin","Demiroð",LocalDate.of(1985,1,6),"28861499108"));
+
+		System.out.println();
+	}
+
+}
